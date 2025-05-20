f56b6da7656f4959791530679cc42844a7ad3ac0
korenevskaya-a <korenevskaya-a@speechpro.com>
2025-05-21 00:24:39 +0300

    added wav2vec_bert test script

---
+++ scripts/itmo-personification-model-large.sh
@@ -0,0 +1,8 @@
+
+export HF_TOKEN=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
+
+
+CUDA_VISIBLE_DEVICES=0,1,2; \
+python -m VoicePersonification.main \
+    -cp=../experiments/itmo-personification-model-large \
+    -cn=test
